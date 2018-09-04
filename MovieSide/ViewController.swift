cfdba86855f48dc035004e56f4ed0ad553b40ea3
ahmetws <ahmetyk@gmail.com>
2018-09-04 20:37:39 +0100

    Initial Commit

---
+++ MovieSide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieSide
+//
+//  Created by Ahmet Yalcinkaya on 4.09.2018.
+//  Copyright © 2018 AppSide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
